a2763247d2bcd39543982c1b379dc6622b8b79a7
Rushil <716548@students.wits.ac.za>
2017-09-07 14:39:39 +0200

    added v1 code to make LED flicker on Serial

---
+++ StimulusSide/arduinoLib/FixedFrequencyAndDutyOnSerial/FixedFrequencyAndDutyOnSerial.ino
@@ -0,0 +1,73 @@
+// Script will flicker an LED on port 13 based on Serial input
+// the frequency will only take effect once a high and low time is given
+// input in form 'u200' and 'd340'
+// must send a pair of commands at present.
+
+int downTicks = 50;
+int upTicks = 50;
+bool writingUp = false;
+bool writingLow = false;
+String downString = "";
+String upString = "";
+bool upDone = false;
+bool lowDone = false;
+
+void setup(){
+  Serial.begin(9600);
+  while(!Serial){}
+  pinMode(13, OUTPUT);
+  downString.reserve(20);
+  upString.reserve(20);
+}
+
+void loop(){
+  digitalWrite(13, HIGH);
+  delay(upTicks);
+  digitalWrite(13, LOW);
+  delay(downTicks);  
+
+  if (upDone && lowDone){ // only enters this routine when a new pair is obtained
+    upDone = false;
+    lowDone = false;
+    downTicks = downString.toInt();
+    upTicks = upString.toInt();
+    upString ="";
+    downString="";
+  }
+}
+
+void serialEvent(){
+  while(Serial.available()){
+    char inByte = Serial.read();
+    Serial.print(inByte);
+    if (inByte == 'u'){ // determines wether to set length of up time
+      writingUp = true;
+    }
+    if (inByte == 'd'){ // determines if the down time must be sent
+      writingLow = true;
+    }
+
+    if (isdigit(inByte)){
+      if (writingUp){
+         upString +=(char)inByte;
+        }
+      if (writingLow){
+        downString +=(char)inByte;
+      }
+    }
+    
+    if (inByte == '\n' && writingUp){ //reset the strings 
+      writingUp = false;
+      upDone = true;
+    }
+    if (inByte == '\n' && writingLow){ //reset the strings 
+      writingLow = false;
+      lowDone = true;
+    }   
+  }
+}
+
+
+
+
+
